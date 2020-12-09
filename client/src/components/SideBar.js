0315eeccbfff5337b9a864fb8267a9724dc84526
Yue Su <su2047@gmail.com>
2020-12-09 14:41:11 -0400

    refined UI

--- client/src/components/SideBar.js
+++ client/src/components/SideBar.js
@@ -7,6 +7,7 @@ import { makeStyles } from "@material-ui/core/styles";
 const useStyles = makeStyles(() => ({
   sideBar: {
     backgroundImage: `linear-gradient(rgba(58, 141, 255, 0.85), rgba(134, 185, 255, 0.85) ),url(${backgroundImage})`,
+    backgroundSize: "cover",
   },
 }));
 
