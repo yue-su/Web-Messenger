b93fa43ed2bb1af796f1526407ffee3328be4305
Yue Su <su2047@gmail.com>
2020-12-13 17:46:46 -0400

    Update UsersProvider.js
    added comments

--- client/src/providers/UsersProvider.js
+++ client/src/providers/UsersProvider.js
@@ -8,6 +8,10 @@ let socket;
 
 export const userContext = createContext();
 
+/**
+ * setting up the initial root states for the app
+ */
+
 const currentChatReceiverInit = {
   userId: "",
   username: "",
@@ -31,6 +35,10 @@ const errorsInit = {
   chatInput: false,
 };
 
+/**
+ * The UsersProveder holds the root states and initalizes them with uselocalstorage
+ */
+
 const UsersProvider = ({ children }) => {
   const [user, setUser] = useLocalStorage("user", userInit);
   const [conversations, setConversations] = useLocalStorage(
@@ -48,14 +56,22 @@ const UsersProvider = ({ children }) => {
 
   const [errors, setErrors] = useState(errorsInit);
 
+  //
+  /**
+   * a currentIdRef is set to track the current conversation's ID on every render.
+   * This is for the socket to check the incoming message's id and compare with the currentIdRef.
+   *
+   * As the socket is initiated in a useEffect only only runs after the first render,
+   * it can't reference any of the state's current value, that's why the currentIdRef took place.
+   */
   const currentIdRef = useRef();
-  //const currentUserRef = useRef(user.userId);
-
   useEffect(() => {
     currentIdRef.current = currentChatReceiver.conversationId;
-    //currentUserRef.current = user.userId;
   });
 
+  /**
+   * socket client set up, the URL is pointing to the backend(on another server)
+   */
   useEffect(() => {
     socket = io("http://192.168.1.11:3001", {
       auth: {
@@ -63,6 +79,13 @@ const UsersProvider = ({ children }) => {
       },
     });
 
+    /**
+     * a replyMessage event is to license the incoming messages sent from the server.
+     * it checks the incoming message's conversationId with the current conversationID.
+     * if the Ids are match, the message will be added to the current chat window.
+     * if it's not a match, the message will be sent to a state called incomingMsg, the state is
+     * passed to the conversationCard for further process.
+     */
     socket.on("replyMessage", (message) => {
       if (message.conversationId === currentIdRef.current) {
         setCurrentChatReceiver((currentChatReceiver) => {
@@ -76,18 +99,30 @@ const UsersProvider = ({ children }) => {
       }
     });
 
+    /**
+     * the getConversation event is checking if another user started a new conversation with the current user
+     */
     socket.on("getConversation", (conversation) => {
-      console.log(conversation);
       setConversations((conversations) => [conversation, ...conversations]);
     });
   }, []);
 
+  /**
+   * this cleanChat function is used by the searchBar.
+   * when the user start a new conversation, it will set the current chat window with an empty array.
+   */
   function cleanChat() {
     setCurrentChatReceiver({
       ...currentChatReceiver,
       messages: [],
     });
   }
+
+  /**
+   * login and register function takes care of the user state,
+   * they also emit a user online message to the socket server.
+   * needs to do a refactor for better structrue
+   */
 
   function login(state, history) {
     axiosWithAuth()
@@ -153,6 +188,11 @@ const UsersProvider = ({ children }) => {
     }
   }
 
+  /**
+   * this passMessages function is used by the conversationCard component.
+   * when a user click one of the conversation card on the list, the receiver's information
+   * will be passed to the current chat window
+   */
   function passMessages(messages, username, conversationId, userId) {
     setCurrentChatReceiver({
       messages: messages,
@@ -162,6 +202,9 @@ const UsersProvider = ({ children }) => {
     });
   }
 
+  /**
+   * When the user sent a message, it will render on the current chat window
+   */
   function renderMessage(message) {
     setCurrentChatReceiver({
       ...currentChatReceiver,
@@ -169,6 +212,11 @@ const UsersProvider = ({ children }) => {
     });
   }
 
+  /**
+   * renderMessages is used by the searchBar
+   * when a new conversation is setup
+   * this one needs a refactor as the axios call seems not necessary.
+   */
   function renderMessages(userTalkToId, userTalkToUsername, newConversationId) {
     axiosWithAuth()
       .get(`/messages/conversation/${userTalkToId}`)
