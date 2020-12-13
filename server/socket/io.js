aa7ffe4193b2b84f0055b5e8a86f2f0f7f7228a7
Yue Su <su2047@gmail.com>
2020-12-12 22:46:51 -0400

    Update io.js

--- server/socket/io.js
+++ server/socket/io.js
@@ -42,6 +42,7 @@ module.exports = (server) => {
     });
 
     socket.on("addConversation", (data) => {
+      console.log(data);
       const currentChatReceiverId = data.currentChatReceiverId;
 
       socket.emit("getConversation", data.data[0]);
