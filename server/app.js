bcd71f39e7ee6832464021ad879ee9e4733f8057
Yue Su <su2047@gmail.com>
2020-12-15 17:51:18 -0400

    clean up logs

--- server/app.js
+++ server/app.js
@@ -1,6 +1,3 @@
-require("dotenv").config();
-
-var http = require("http");
 const createError = require("http-errors");
 const cookieParser = require("cookie-parser");
 const { join } = require("path");
@@ -9,51 +6,35 @@ const cors = require("cors");
 const express = require("express");
 const { json, urlencoded } = express;
 const app = express();
-const { onError, normalizePort } = require("./utils/server");
+
 const { socketAuth } = require("./middlewares/auth");
-const {
-  addUserToMap,
-  removeUserFromMap,
-  userSocketIdMap,
-} = require("./utils/userSocketIdMap");
+const { addUserToMap, removeUserFromMap } = require("./utils/userSocketIdMap");
 
-var port = normalizePort(process.env.PORT || "3001");
-app.set("port", port);
-
-const server = http.createServer(app);
-server.listen(port);
-server.on("error", onError);
-server.on("listening", onListening);
-
-const io = require("socket.io")(server, {
+const io = require("socket.io")({
   cors: {
     origin: "*",
     methods: ["GET", "POST"],
   },
 });
+app.io = io;
 
 io.use(socketAuth);
 
 io.on("connection", (socket) => {
-  console.log("new socket connection");
-
   //when a user is connected, the userId and socketId will be added a in memory map
   socket.on("online", (user) => {
     socket.userId = user.userId;
     addUserToMap(user.userId, socket.id);
-    console.log(userSocketIdMap);
   });
 
   //delete the user from the map when it's offline
   socket.on("disconnect", () => {
     removeUserFromMap(socket.userId);
-    console.log(userSocketIdMap);
   });
 });
 
-//const indexRouter = require("./routes/index");
-//const pingRouter = require("./routes/ping");
-
+const indexRouter = require("./routes/index");
+const pingRouter = require("./routes/ping");
 const usersRouter = require("./routes/users");
 const conversationsRouter = require("./routes/conversations")(io);
 const messageRouter = require("./routes/messages")(io);
@@ -65,8 +46,8 @@ app.use(urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(join(__dirname, "public")));
 
-//app.use("/", indexRouter);
-//app.use("/ping", pingRouter);
+app.use("/", indexRouter);
+app.use("/ping", pingRouter);
 app.use("/api/users", usersRouter);
 app.use("/api/conversations", conversationsRouter);
 app.use("/api/messages", messageRouter);
@@ -87,9 +68,4 @@ app.use(function (err, req, res, next) {
   res.json({ error: err });
 });
 
-function onListening() {
-  var addr = server.address();
-  var bind = typeof addr === "string" ? "pipe " + addr : "port " + addr.port;
-
-  console.log("Listening on " + bind);
-}
+module.exports = app;
