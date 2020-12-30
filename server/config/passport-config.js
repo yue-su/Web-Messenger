3c6399df599eaf80286aca5e57ea2a4c2531ce95
Yue Su <su2047@gmail.com>
2020-12-30 01:11:57 -0400

    removed clientID

--- server/config/passport-config.js
+++ server/config/passport-config.js
@@ -1,21 +1,22 @@
 var passport = require("passport");
 var GoogleStrategy = require("passport-google-oauth").OAuth2Strategy;
 
+const { models } = require("../models");
+const { user } = models;
+
 // Use the GoogleStrategy within Passport.
 //   Strategies in passport require a `verify` function, which accept
 //   credentials (in this case, a token, tokenSecret, and Google profile), and
 //   invoke a callback with a user object.
-
 passport.use(
   new GoogleStrategy(
     {
-      clientID:
-        "669322067793-0uevk0npdkv14dpf61bhllqm9v2s0m6v.apps.googleusercontent.com",
-      clientSecret: "2a4VDFVksn8uRXihnpij0Rov",
-      callbackURL: "http://localhost:3001/auth/google/redirect",
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: process.env.GOOGLE_CALLBACK,
     },
     function (token, tokenSecret, profile, done) {
-      console.log("token is: ", token);
+      console.log(token);
     }
   )
 );
